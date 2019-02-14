c5ab82798089fc9d0bd9a18aa2d9b86a014db77b
laoayi <1916541806@qq.com>
2019-02-14 16:40:52 +0800

    用户登录功能实现完毕

--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入global.css全局样子 控制 文件
+import './assets/css/global.css'
+
+// 引入 图标 css 样式文件
+import './assets/fonts/iconfont.css'
+// 引入element-ui组件模块
+import ElementUI from 'element-ui'
+
+// 引入axios 并做相关配置
+import axios from 'axios'
+// axios 请求根地址
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 给axios 配置给vue 的$http 成员
+Vue.prototype.$http = axios
+
+// 把element-ui 注册给vue
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
